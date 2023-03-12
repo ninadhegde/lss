8bc293a6b2823e6177f622150252da98ab04a4d0
ninadhegde <ninadhegde@gmail.com>
2023-03-13 02:09:32 +0530

    Added some garbage commands for practice

--- script02.sh
+++ script02.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 var1="567"
 test='RVCE'
-bool=False
+bool=True
 #comments
 echo " this is value of var : $var1"
 echo "This is test $test"
 echo "Boolian : $bool"
 echo "host : $HOSTNAME"
+mkdir test_shell_script
+cd test_shell_script
+echo "Ollo" > hello.txt
+ca hello.txt
+echo "Hellololo" >> hello.txt
+cat hello.txt
